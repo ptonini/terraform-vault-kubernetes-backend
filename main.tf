83fcad51184dfa373915670779eeedabed7a30ae
Pedro Tonini <51178173+ptonini@users.noreply.github.com>
2022-12-08 21:28:29 -0300

    feat: added root credentials rotation

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
 module "user_certificate" {
-  source = "ptonini/user-certificate/kubernetes"
+  source  = "ptonini/user-certificate/kubernetes"
   version = "~> 1.0.0"
-  name = var.user_certificate_name
+  name    = var.user_certificate_name
   cluster_role_rules = [
     {
       api_groups = ["rbac.authorization.k8s.io"]
-      resources = ["roles", "clusterroles", "rolebindings", "clusterrolebindings"]
-      verbs = ["*"]
+      resources  = ["roles", "clusterroles", "rolebindings", "clusterrolebindings"]
+      verbs      = ["*"]
     },
     {
       api_groups = ["certificates.k8s.io"]
-      resources = ["certificatesigningrequests", "certificatesigningrequests/approval"]
-      verbs = ["*"]
+      resources  = ["certificatesigningrequests", "certificatesigningrequests/approval"]
+      verbs      = ["*"]
     },
     {
-      api_groups = ["certificates.k8s.io"]
-      resources = ["signers"]
+      api_groups    = ["certificates.k8s.io"]
+      resources     = ["signers"]
       resourceNames = ["kubernetes.io/kube-apiserver-client"]
-      verbs = ["approve", "sign" ]
+      verbs         = ["approve", "sign"]
     }
   ]
   providers = {
@@ -25,24 +25,33 @@ module "user_certificate" {
   }
 }
 
-module "vault_mount"{
-  source = "ptonini/mount/vault"
+module "vault_mount" {
+  source  = "ptonini/mount/vault"
   version = "~> 1.0.0"
-  path = var.path
-  type = var.type
+  path    = var.path
+  type    = var.type
 }
 
-resource "vault_generic_endpoint" "config" {
-  path = "${module.vault_mount.this.path}/config"
+resource "vault_generic_endpoint" "this" {
+  path                 = "${module.vault_mount.this.path}/config"
   ignore_absent_fields = true
-  disable_read = true
-  disable_delete = true
+  disable_read         = true
+  disable_delete       = true
   data_json = jsonencode({
-    host = var.host
-    ca_cert = var.ca_cert
+    host        = var.host
+    ca_cert     = var.ca_cert
     client_cert = module.user_certificate.this.certificate
-    client_key = module.user_certificate.private_key.private_key_pem
+    client_key  = module.user_certificate.private_key.private_key_pem
     default_ttl = var.default_ttl
-    max_ttl = var.max_ttl
+    max_ttl     = var.max_ttl
   })
+}
+
+resource "null_resource" "rotate_role_password" {
+  provisioner "local-exec" {
+    command = "VAULT_TOKEN=${var.vault_token} vault write -force ${module.vault_mount.this.path}/rotate-root"
+  }
+  depends_on = [
+    vault_generic_endpoint.this
+  ]
 }
