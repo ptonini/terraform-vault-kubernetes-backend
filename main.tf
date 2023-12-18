2135ba8baefe2f17be6f028f7cb05150e2bfddb4
ptonini <pedro.tonini@hotmail.com>
2023-12-18 03:15:44 -0300

    feat!: replaced resources for default kubernetes backend implementation

--- main.tf
+++ main.tf
@@ -1,93 +1,23 @@
-locals {
-  certificate_rules = [
-    {
-      api_groups = ["certificates.k8s.io"]
-      resources  = ["certificatesigningrequests", "certificatesigningrequests/approval"]
-      verbs      = ["*"]
-    },
-    {
-      api_groups    = ["certificates.k8s.io"]
-      resources     = ["signers"]
-      resourceNames = ["kubernetes.io/kube-apiserver-client"]
-      verbs         = ["approve", "sign"]
-    }
-  ]
-  serviceaccount_rules = [
-    {
-      api_groups : [""]
-      resources : ["serviceaccounts", "secrets"]
-      verbs : ["*"]
-    }
-  ]
-  all_rules = concat(
-    [
-      {
-        api_groups = ["rbac.authorization.k8s.io"]
-        resources  = ["roles", "clusterroles", "rolebindings", "clusterrolebindings"]
-        verbs      = ["*"]
-      }
-    ],
-    var.certificate_generation ? local.certificate_rules : [],
-    var.serviceaccount_generation ? local.serviceaccount_rules : []
-  )
+resource "vault_kubernetes_secret_backend" "this" {
+  path                      = var.path
+  default_lease_ttl_seconds = var.default_lease_ttl_seconds
+  max_lease_ttl_seconds     = var.max_lease_ttl_seconds
+  kubernetes_host           = var.kubernetes_host
+  kubernetes_ca_cert        = var.kubernetes_ca_cert
+  service_account_jwt       = var.service_account_jwt
+  disable_local_ca_jwt      = var.disable_local_ca_jwt
 }
 
-module "service_account" {
-  source             = "ptonini/service-account/kubernetes"
-  version            = "~> 1.2.0"
-  count              = var.credentials == "service_account" ? 1 : 0
-  name               = var.username
-  namespace          = "kube-system"
-  cluster_role_rules = local.all_rules
-  create_bindings = var.create_bindings
-  providers = {
-    kubernetes = kubernetes
-  }
-}
-
-module "certificate" {
-  source             = "ptonini/user-certificate/kubernetes"
-  version            = "~> 1.2.0"
-  count              = var.credentials == "certificate" ? 1 : 0
-  name               = var.username
-  cluster_role_rules = local.all_rules
-  create_bindings = var.create_bindings
-  providers = {
-    kubernetes = kubernetes
-  }
-}
-
-module "vault_mount" {
-  source  = "ptonini/mount/vault"
-  version = "~> 1.0.0"
-  path    = var.path
-  type    = var.type
-}
-
-resource "vault_generic_endpoint" "this" {
-  path                 = "${module.vault_mount.this.path}/config"
-  ignore_absent_fields = true
-  disable_read         = true
-  disable_delete       = true
-  data_json = jsonencode({
-    host        = var.host
-    ca_cert     = try(module.service_account[0].ca_crt, var.ca_cert)
-    token       = try(module.service_account[0].token, var.token)
-    client_cert = try(module.certificate[0].this.certificate, var.client_cert)
-    client_key  = try(module.certificate[0].private_key.private_key_pem, var.client_key)
-    default_ttl = var.default_ttl
-    max_ttl     = var.max_ttl
-  })
-}
-
-resource "vault_generic_endpoint" "rotate_root" {
-  count                = var.rotate_root ? 1 : 0
-  path                 = "${module.vault_mount.this.path}/rotate-root"
-  ignore_absent_fields = true
-  disable_read         = true
-  disable_delete       = true
-  data_json            = "{}"
-  depends_on = [
-    vault_generic_endpoint.this
-  ]
+resource "vault_kubernetes_secret_backend_role" "this" {
+  for_each                      = var.roles
+  backend                       = vault_kubernetes_secret_backend.this.path
+  name                          = coalesce(each.value.name, each.key)
+  allowed_kubernetes_namespaces = each.value.allowed_kubernetes_namespaces
+  token_max_ttl                 = each.value.token_max_ttl
+  token_default_ttl             = each.value.token_default_ttl
+  service_account_name          = each.value.service_account_name
+  kubernetes_role_name          = each.value.kubernetes_role_name
+  kubernetes_role_type          = each.value.kubernetes_role_type
+  extra_labels                  = each.value.extra_labels
+  extra_annotations             = each.value.extra_annotations
 }
--- variables.tf
+++ variables.tf
@@ -1,56 +1,92 @@
 variable "path" {}
 
-variable "type" {
-  default = "kubernetes_v2"
-}
+variable "kubernetes_host" {}
 
-variable "host" {}
+variable "service_account_jwt" {}
 
-variable "credentials" {
-  type    = string
-  default = "none"
-}
+variable "kubernetes_ca_cert" {}
 
-variable "username" {
-  default = "vault-backend"
-}
-
-variable "token" {
-  default = null
-}
-
-variable "ca_cert" {
-  default = null
-}
-
-variable "client_cert" {
-  default = null
-}
-
-variable "client_key" {
-  default = null
-}
-
-variable "default_ttl" {
-  default = null
-}
-
-variable "max_ttl" {
-  default = null
-}
-
-variable "rotate_root" {
+variable "disable_local_ca_jwt" {
   default = false
 }
 
-variable "serviceaccount_generation" {
-  default = false
+variable "default_lease_ttl_seconds" {
+  default = null
 }
 
-variable "certificate_generation" {
-  default = true
+variable "max_lease_ttl_seconds" {
+  default = null
 }
 
-variable "create_bindings" {
-  default = false
+variable "roles" {
+  type = map(object({
+    name                          = optional(string)
+    allowed_kubernetes_namespaces = optional(set(string), ["*"])
+    token_max_ttl                 = optional(number)
+    token_default_ttl             = optional(number)
+    service_account_name          = optional(string)
+    kubernetes_role_name          = optional(string)
+    kubernetes_role_type          = optional(string)
+    generated_role_rules          = optional(string)
+    extra_labels                  = optional(map(string))
+    extra_annotations = optional(map(string))
+  }))
+  default = {}
 }
+
+
+
+#variable "type" {
+#  default = "kubernetes_v2"
+#}
+#
+#variable "host" {}
+#
+#variable "credentials" {
+#  type    = string
+#  default = "none"
+#}
+#
+#variable "username" {
+#  default = "vault-backend"
+#}
+#
+#variable "token" {
+#  default = null
+#}
+#
+#variable "ca_cert" {
+#  default = null
+#}
+#
+#variable "client_cert" {
+#  default = null
+#}
+#
+#variable "client_key" {
+#  default = null
+#}
+#
+#variable "default_ttl" {
+#  default = null
+#}
+#
+#variable "max_ttl" {
+#  default = null
+#}
+#
+#variable "rotate_root" {
+#  default = false
+#}
+#
+#variable "serviceaccount_generation" {
+#  default = false
+#}
+#
+#variable "certificate_generation" {
+#  default = true
+#}
+#
+#variable "create_bindings" {
+#  default = false
+#}
