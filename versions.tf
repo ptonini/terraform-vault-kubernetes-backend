5dae01a40a809f628c99a668754940e9026bb90a
ptonini <pedro.tonini@hotmail.com>
2023-12-18 03:37:47 -0300

    fix: removed kubernetes provider requirement

--- versions.tf
+++ versions.tf
@@ -2,10 +2,7 @@ terraform {
   required_providers {
     vault = {
       source  = "hashicorp/vault"
-      version = ">= 3.11.0"
-    }
-    kubernetes = {
-      source = "hashicorp/kubernetes"
+      version = ">= 3.23.0"
     }
   }
 }
