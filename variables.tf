b7bcf7183be9cd3370e9cf845c257bfda5124d09
ptonini <pedro.tonini@hotmail.com>
2023-12-18 03:20:52 -0300

    fix: refactored variables.tf

--- variables.tf
+++ variables.tf
@@ -8,6 +8,7 @@ variable "kubernetes_ca_cert" {}
 
 variable "disable_local_ca_jwt" {
   default = false
+  nullable = false
 }
 
 variable "default_lease_ttl_seconds" {
@@ -33,60 +34,3 @@ variable "roles" {
   }))
   default = {}
 }
-
-
-
-#variable "type" {
-#  default = "kubernetes_v2"
-#}
-#
-#variable "host" {}
-#
-#variable "credentials" {
-#  type    = string
-#  default = "none"
-#}
-#
-#variable "username" {
-#  default = "vault-backend"
-#}
-#
-#variable "token" {
-#  default = null
-#}
-#
-#variable "ca_cert" {
-#  default = null
-#}
-#
-#variable "client_cert" {
-#  default = null
-#}
-#
-#variable "client_key" {
-#  default = null
-#}
-#
-#variable "default_ttl" {
-#  default = null
-#}
-#
-#variable "max_ttl" {
-#  default = null
-#}
-#
-#variable "rotate_root" {
-#  default = false
-#}
-#
-#variable "serviceaccount_generation" {
-#  default = false
-#}
-#
-#variable "certificate_generation" {
-#  default = true
-#}
-#
-#variable "create_bindings" {
-#  default = false
-#}
